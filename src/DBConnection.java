0dfc7b618b481c4e961af1a6c5724822650d4584
Tenuka <tenuka0506@gmail.com>
2026-07-22 14:53:36 +0530

    Update database connect method

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -2,23 +2,15 @@ package src;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class DBConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/Tasks";
     private static final String USER = "postgres";
     private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
-    public static Connection connect() {
-        try {
-            return DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+    public static Connection connect() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
-
-
-
-
-
--- src/mainWindow.java
+++ src/mainWindow.java
@@ -7,7 +7,6 @@ import javax.swing.table.TableCellRenderer;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
